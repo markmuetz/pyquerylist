a5f6d192cea865f9e0f1fe076397e9cbec45cea0
Mark Muetzelfeldt <markmuetz@gmail.com>
2022-04-21 13:01:29 +0100

    Initial commit. Usable collection of classes to implement QueryList.
    QueryList - list that you can query using standard methods.
    Query - Query that speficies which items to choose.
    FuncQuery - as Query but allows you to use lambda.
    MultiQuery - combined (by e.g. & |) Query objs.
    QueryGroup - result of calling QueryList.groupby.

---
+++ querylist/__init__.py
@@ -0,0 +1,10 @@
+from querylist.querylist import Query, FuncQuery, QueryList, QueryGroup
+from querylist.version import VERSION
+
+__version__ = VERSION
+__all__ = [
+    'Query',
+    'FuncQuery',
+    'QueryList',
+    'QueryGroup',
+]
